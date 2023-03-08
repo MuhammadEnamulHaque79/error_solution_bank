a912922bb41e076c45c911a3a5afe4f86fcdf9d4
Enamul <muhammad.enamul.haque79@gmail.com>
2023-03-08 12:27:16 +0600

    first commit

---
+++ _debug_steps.js
@@ -0,0 +1,8 @@
+/**
+ Debugging steps;
+ 1.go to live website & try to reproduce the error;
+ 2.check the console of the live website;
+ 3.read exception thoroughly in details;
+ 4.the error is brand new then copy and search the google;
+ 5.
+ */
